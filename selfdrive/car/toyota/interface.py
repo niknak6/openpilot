67a8caa6ca52bc3b1e906d1f6f1d7ef071e3b5a1
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-09-05 14:23:07 -0700

    September 5th, 2024 Update

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -134,10 +134,10 @@ class CarInterface(CarInterfaceBase):
 
     tune = ret.longitudinalTuning
     if params.get_bool("ToyotaTune"):
-      tune.kiBP = [5., 35.]
-      tune.kiV = [1.5, 0.5]
-      ret.vEgoStopping = 0.15
-      ret.vEgoStarting = 0.15
+      tune.kiBP = [0., 5., 35.]
+      tune.kiV = [1.0, 1.5, 0.5]
+      ret.vEgoStopping = 0.25
+      ret.vEgoStarting = 0.25
       ret.stoppingDecelRate = 0.1  # reach stopping target smoothly
     elif candidate in TSS2_CAR:
       tune.kpV = [0.0]
